56d54777b0882d2537c708f7516dcae3fadc71ea
Aman Vishwakarma <92659449+vishamanofficial@users.noreply.github.com>
2024-02-27 10:26:18 +0530

    Update Banner.js

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -22,9 +22,6 @@ const Banner = () => {
         <img src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_288,h_360/v1674029857/PC_Creative%20refresh/3D_bau/banners_new/Poha.png"></img>
       </div>
       <div>
-        <img src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_288,h_360/v1674029857/PC_Creative%20refresh/3D_bau/banners_new/Poha.png"></img>
-      </div>
-      <div>
         <img src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_288,h_360/v1674029855/PC_Creative%20refresh/3D_bau/banners_new/Poori.png"></img>
       </div>
     </div>
