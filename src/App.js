d7655dd147ffc69ae4fdde5518866cc5ddbb0a66
Aman Vishwakarma <92659449+vishamanofficial@users.noreply.github.com>
2024-02-28 11:48:40 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-
+import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Body from "./components/Body";
 import Error from "./components/Error";
@@ -12,6 +12,7 @@ const AppLayout = () => {
     <div className="app">
       <Header />
       <Outlet />
+      <Footer />
     </div>
   );
 };
